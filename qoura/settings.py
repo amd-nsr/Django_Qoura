77597ac1a5e9d68dc920c1b9741465eb2c1ece50
b-e-d-o <36853269+b-e-d-o@users.noreply.github.com>
2020-03-31 12:08:25 +0200

    Allowed Hosts Adding

--- qoura/settings.py
+++ qoura/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['qoura-aaa.herokuapp.com','127.0.0.1']
 AUTH_USER_MODEL = 'core.User'
 
 
